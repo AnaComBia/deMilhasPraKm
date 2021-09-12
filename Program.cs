564b81e36d3a149c88da35d96467b2468d44a523
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-11 21:14:46 -0300

    finaliza programa De Milhas pra Km

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace deMilhasPraKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nfale a unidade em milhas que eu mostrarei o resultado em Km: ");
+            double ValorMilhas = Convert.ToDouble(Console.ReadLine());
+
+            double ValorKm = ValorMilhas * 1.609;
+
+            Console.WriteLine($"\nvalor em Km: {ValorKm}\n");
+        }
+    }
+}
